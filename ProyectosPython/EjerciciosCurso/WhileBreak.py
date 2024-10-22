0885225c119b50e41b830488cf1efd5d75883b31
Sehoul <sehoul.informatica@gmail.com>
2024-10-23 01:07:49 +0200

    Primera ronda

---
+++ ProyectosPython/EjerciciosCurso/WhileBreak.py
@@ -0,0 +1,8 @@
+clave = "chupacabras"
+
+usuario = input("Introduce una palabra\n")
+while clave != usuario:
+    print("Has fallado")
+    usuario = input("Introduce una palabra\n")
+
+print("Fin del programa")
